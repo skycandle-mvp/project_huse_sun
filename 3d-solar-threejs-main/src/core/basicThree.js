82fe538d68920e507cfd0e576cc1a080868c5b86
liangyishiasd <1205165440@qq.com>
2024-11-29 23:28:50 +0800

    chushihua

--- 3d-solar-threejs-main/src/core/basicThree.js
+++ 3d-solar-threejs-main/src/core/basicThree.js
@@ -9,23 +9,23 @@ export class basicThree {
   constructor() {
     this.container = document.querySelector('#sunshine')
     this.modelScale = 1 // 模型缩放倍数
-    this.modelUrl // 模型URL
+    this.modelUrl = null // 模型URL
 
     // three 3要素
-    this.renderer // 渲染器
-    this.camera // 相机
-    this.scene // 场景
+    this.renderer = null
+    this.camera = null
+    this.scene = null
 
-    //光源
-    this.ambientLight // 环境光
-    this.sunLight // 太阳光
+    // 光源
+    this.ambientLight = null
+    this.sunLight = null
 
-    //摄像头控制
-    this.controls
+    // 摄像头控制
+    this.controls = null
     this.renderEvents = []
 
     // 记录原始的旋转中心点
-    this.originalTarget
+    this.originalTarget = null
 
     this.init()
   }
@@ -34,162 +34,160 @@ export class basicThree {
     this.initScene()
     this.initCamera()
     this.initRender()
-
     this.orbitHelper()
-    this.statsHelper() //性能辅助
+    this.statsHelper()
     this.animate()
 
     window.onresize = this.onWindowResize.bind(this)
   }
 
+  // 场景初始化
   initScene() {
     this.scene = new THREE.Scene()
-    this.scene.background = new THREE.Color('#b2e0ff')
-    // this.scene.add(new THREE.AmbientLight(0xffffff, 1))
+    this.scene.background = new THREE.Color('#eef5ff')
 
-    // 半球光,会比环境光颜色更自然
-    const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1) // (sky color, floor color)
-    this.scene.add(hemisphereLight)
-
-    this.scene.receiveShadow = true
+    // 使用环境光照亮整个场景
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+    this.scene.add(ambientLight)
   }
 
-  initCamera() {
-    this.camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      1,
-      3000,
-    )
-    // 相机所在的位置，这是示例,最终的位置还得根据小区大小来选择,如果小区较大
-    // 则相机需要更远, fov可能需要更大
-    this.camera.position.set(0, 500, 500)
-    this.camera.lookAt(new THREE.Vector3(0, 0, 0))
-  }
-
+  // 渲染器初始化
   initRender() {
-    const renderer = new THREE.WebGLRenderer({
-      antialias: true, // 抗锯齿
-      logarithmicDepthBuffer: true,
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
       alpha: true,
+      logarithmicDepthBuffer: true,
     })
 
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.setPixelRatio(1)
-    //告诉渲染器需要阴影效果
-    renderer.shadowMap.enabled = true
-    //RGB模式编码（sRGBEncoding）进行对材质进行渲染,SRGBColorSpace
-    renderer.outputEncoding = THREE.SRGBColorSpace
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap
-    renderer.toneMapping = THREE.ReinhardToneMapping
-    // 默认1为了让场景更明亮
-    renderer.toneMappingExposure = 1
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.outputColorSpace = THREE.SRGBColorSpace
+    this.renderer.shadowMap.enabled = true
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-    this.container.appendChild(renderer.domElement)
-    this.renderer = renderer
+    this.container.appendChild(this.renderer.domElement)
   }
 
-  initLight() {
-    this.sunLight = new THREE.DirectionalLight(0xffffff)
-    this.sunLight.visible = true
-    this.sunLight.intensity = 20 //光线的密度，默认为1。 光照越强，物体表面就更明亮
-    this.sunLight.shadow.camera.near = -1000 //产生阴影的最近距离
-    this.sunLight.shadow.camera.far = 1000 //产生阴影的最远距离
-    this.sunLight.shadow.camera.left = -1000 //产生阴影距离位置的最左边位置
-    this.sunLight.shadow.camera.right = 1000 //最右边
-    this.sunLight.shadow.camera.top = 1000 //最上边
-    this.sunLight.shadow.camera.bottom = -1000 //最下面
-    this.sunLight.shadow.bias = -0.01 //用于解决阴影水波纹条纹阴影的问题
-    this.sunLight.shadow.mapSize.set(4096, 4096) //阴影清晰度
+  // 相机初始化
+  initCamera() {
+    this.camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        1,
+        3000
+    )
+    this.camera.position.set(200, 300, 300)
+    this.camera.lookAt(0, 0, 0)
+  }
 
-    //告诉平行光需要开启阴影投射,物体遮挡阴影
+  // 光照初始化
+  initLight() {
+    this.sunLight = new THREE.DirectionalLight(0xffffff, 1)
+    this.sunLight.position.set(0, 100, 0)
     this.sunLight.castShadow = true
+
+    this.sunLight.shadow.mapSize.width = 2048
+    this.sunLight.shadow.mapSize.height = 2048
+    this.sunLight.shadow.camera.near = 1
+    this.sunLight.shadow.camera.far = 1000
+    this.sunLight.shadow.camera.left = -500
+    this.sunLight.shadow.camera.right = 500
+    this.sunLight.shadow.camera.top = 500
+    this.sunLight.shadow.camera.bottom = -500
+
     this.scene.add(this.sunLight)
   }
 
-  /** 加载模型 */
+  // 加载3D模型
   loadModel() {
     const loader = new GLTFLoader()
-    /**
-     * DRACOLoader 解码器介绍:
-     * 在Three.js中，加载GLB模型时是否需要DRACOLoader解码器取决于您的模型文件是否使用了Draco压缩。
-     * DRACOLoader是一个用于解码Draco压缩网格数据的加载器。
-     * 如果您的GLB模型文件中的几何体数据已经通过 Draco 压缩过，那么在加载时就需要使用DRACOLoader进行解码。
-     * 如果没有进行Draco压缩，那么直接使用GLTFLoader即可加载，无需额外设置DRACOLoader。
-     */
-    /**
-     * 如果您使用gltf-pipeline工具对模型进行了Draco压缩，
-     * 那么在加载这个压缩后的GLTF或GLB文件时，
-     * 就需要用到DRACOLoader来解码Draco压缩过的几何数据。
-     */
-
     const dracoLoader = new DRACOLoader()
 
-    /**
-     * 设置Draco解码库
-     * Path: node_modules/three/examples/jsm/libs/draco文件复制到public文件下
-     */
     dracoLoader.setDecoderPath('./draco/')
-    dracoLoader.setDecoderConfig({ type: 'js' }) // 使用js方式解压
-    dracoLoader.preload() // 初始化_initDecoder 解码器
-    loader.setDRACOLoader(dracoLoader) // 设置gltf加载器dracoLoader解码器
+    dracoLoader.setDecoderConfig({ type: 'js' })
+    dracoLoader.preload()
+    loader.setDRACOLoader(dracoLoader)
 
     loader.load(
-      this.modelUrl,
-      (gltf) => {
-        const model = gltf.scene
+        this.modelUrl,
+        (gltf) => {
+          const model = gltf.scene
 
-        // 遍历模型中的所有子对象，设置阴影接收和投射属性
-        model.traverse((child) => {
-          if (child.isMesh) {
-            const copyMaterial = child.material.clone()
-            copyMaterial.side = THREE.DoubleSide
-            copyMaterial.originColor = copyMaterial.color.clone()
-            copyMaterial.color.setHex(0xfffff0)
-            child.material = copyMaterial
+          model.traverse((child) => {
+            if (child.isMesh) {
+              const copyMaterial = child.material.clone()
+              copyMaterial.side = THREE.DoubleSide
+              copyMaterial.originColor = copyMaterial.color.clone()
+              copyMaterial.color.setHex(0xfffff0)
+              child.material = copyMaterial
 
-            //物体遮挡阴影
-            child.castShadow = true
-            child.receiveShadow = true
-          }
-        })
+              child.castShadow = true
+              child.receiveShadow = true
+            }
+          })
 
-        model.scale.set(this.modelScale, this.modelScale, this.modelScale)
-        this.building = gltf.scene
-        this.scene.add(gltf.scene)
-      },
-      undefined,
-      function (error) {
-        console.error(error)
-      },
+          model.scale.set(this.modelScale, this.modelScale, this.modelScale)
+          this.building = gltf.scene
+          this.scene.add(gltf.scene)
+        },
+        undefined,
+        (error) => {
+          console.error('Error loading model:', error)
+        }
     )
   }
 
+  // 初始化模型和地面
   initModel() {
     this.loadModel()
     this.initLight()
     this.basicfloor()
   }
 
-  /**
-   * 轨道控制器
-   * 作用: 用户可以通过鼠标或触摸输入来交互式地旋转、缩放和平移3D视图
-   */
+  // 地面（地图）设置
+  basicfloor() {
+    const planeGeometry = new THREE.PlaneGeometry(500, 500)
+
+    const texture = new THREE.TextureLoader().load(
+        './bg.png',
+        (texture) => {
+          texture.colorSpace = THREE.SRGBColorSpace
+          texture.minFilter = THREE.LinearFilter
+          texture.magFilter = THREE.LinearFilter
+          texture.needsUpdate = true
+
+          texture.wrapS = THREE.ClampToEdgeWrapping
+          texture.wrapT = THREE.ClampToEdgeWrapping
+        },
+        undefined,
+        (error) => {
+          console.error('Error loading texture:', error)
+        }
+    )
+
+    const planeMaterial = new THREE.MeshBasicMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+      transparent: true,
+      opacity: 1,
+    })
+
+    this.plane = new THREE.Mesh(planeGeometry, planeMaterial)
+    this.plane.rotation.x = -Math.PI / 2
+    this.plane.position.y = 0
+
+    this.scene.add(this.plane)
+  }
+
+  // 控制器设置
   orbitHelper() {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-
-    // 使动画循环使用时阻尼或自转 意思是否有惯性
     this.controls.enableDamping = true
+    this.controls.dampingFactor = 0.05
 
-    //是否可以缩放
-    this.controls.enableZoom = true
-    //设置相机距离原点的最远距离
     this.controls.minDistance = 100
-    //设置相机距离原点的最远距离
-    this.controls.maxDistance = 800
-    //设置相机上下旋转最大角度最大到平面
-    this.controls.minPolarAngle = 0
-    this.controls.maxPolarAngle = Math.PI / 2 - 0.2
+    this.controls.maxDistance = 1000
+    this.controls.maxPolarAngle = Math.PI / 2 - 0.1
 
     this.originalTarget = this.controls.target.clone()
 
@@ -202,82 +200,47 @@ export class basicThree {
         z: THREE.MathUtils.radToDeg(rotation.z),
       }
       document.querySelector(".compass>div").style.transform =
-        "rotate(" + rotationDegrees.z + "deg)";
+          "rotate(" + rotationDegrees.z + "deg)"
     })
   }
 
-  // 创建网格辅助
-  gridHelper() {
-    var gridHelper = new THREE.GridHelper(20, 20, 0x404040, 0x404040)
-    this.scene.add(gridHelper)
-  }
-
-  //性能插件
+  // 性能监控
   statsHelper() {
     this.stats = new Stats()
     this.stats.dom.style.top = '100px'
     document.body.appendChild(this.stats.dom)
   }
 
-  //坐标轴辅助
-  axesHelper() {
-    var helper = new THREE.AxesHelper(10)
-    this.scene.add(helper)
-  }
-
+  // 窗口大小变化处理
   onWindowResize() {
     this.camera.aspect = window.innerWidth / window.innerHeight
     this.camera.updateProjectionMatrix()
-    this.render()
     this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.render()
   }
 
+  // 渲染
   render() {
     if (this.stats) this.stats.update()
     this.renderer.render(this.scene, this.camera)
     this.controls.update()
     TWEEN.update()
-    this.renderEvents.forEach((e) => {
-      if (e && typeof e === 'function') {
-        e()
+
+    this.renderEvents.forEach((event) => {
+      if (event && typeof event === 'function') {
+        event()
       }
     })
   }
 
-  /** 注册渲染回调事件,以便外界想要在渲染的时候去执行一些事件 */
+  // 注册渲染事件
   registRenderEvent(event) {
     this.renderEvents.push(event)
   }
 
+  // 动画循环
   animate() {
-    this.render()
     requestAnimationFrame(this.animate.bind(this))
-  }
-
-  // 设置地面
-  basicfloor() {
-    const planeGeometry = new THREE.CircleGeometry(2048)
-    const texture = new THREE.TextureLoader().load(
-      './bg.jpg',
-    )
-    texture.colorSpace = THREE.SRGBColorSpace
-    texture.magFilter = THREE.LinearFilter
-    texture.minFilter = THREE.LinearMipmapLinearFilter
-
-    const planeMaterial = new THREE.MeshStandardMaterial({
-      map: texture,
-      reflectivity: 0, // 反射率
-      roughness: 1, // 粗糙度
-      metalness: 0,
-    })
-
-    this.plane = new THREE.Mesh(planeGeometry, planeMaterial)
-    this.plane.rotation.x = -0.5 * Math.PI
-    this.plane.position.y = 0
-    //物体遮挡阴影
-    this.plane.castShadow = true
-    //告诉底部平面需要接收阴影
-    this.plane.receiveShadow = true
-    this.scene.add(this.plane)
+    this.render()
   }
 }
